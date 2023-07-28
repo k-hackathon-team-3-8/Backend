dd2cd4ccc560ac1bb4596bba21c44d90bbd2dfe9
SoMin Park <ps4708@naver.com>
2023-07-28 18:26:05 +0900

    initial commit

---
+++ src/main/java/org/wakeupu/wakeupu/WakeupUApplication.java
@@ -0,0 +1,13 @@
+package org.wakeupu.wakeupu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakeupUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakeupUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wakeupu/wakeupu/WakeupUApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wakeupu.wakeupu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakeupUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
